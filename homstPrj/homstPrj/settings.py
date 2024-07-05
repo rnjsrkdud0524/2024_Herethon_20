4dfd1628affcc48b9edcee105b33ef023e683883
rnjsrkdud0524 <rjanseh2003@naver.com>
2024-07-05 11:26:54 +0900

    수정확인

--- homstPrj/homstPrj/settings.py
+++ homstPrj/homstPrj/settings.py
@@ -129,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
